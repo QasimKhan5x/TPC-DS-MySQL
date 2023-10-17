fb06e6ca1662be1bd09860a192fec50260cea26a
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2023-10-17 22:19:25 +0200

    add null on terminating

--- scripts/null_values.sh
+++ scripts/null_values.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-# 1_nulls is a copy of the original folder represented by the SF=1
-input_dir="../data/2"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <leaf_directory>"
+    exit 1
+fi
+
+# Take the leaf directory as input
+input_dir="../data/$1"
+
+# Check if directory exists
+if [ ! -d "$input_dir" ]; then
+    echo "Directory $input_dir does not exist!"
+    exit 1
+fi
+
 # Loop over each CSV file in the directory
 for csv_file in "$input_dir"/*.csv; do
   echo "Processing $csv_file..."
+  
+  # Replace occurrences of "||" with "|\\N|"
   sed -i ':a; s/||/|\\N|/g; ta' "$csv_file"
+  
+  # If a line ends with "|", replace it with "|\N"
+  sed -i 's/|$/|\\N/' "$csv_file"
 done
